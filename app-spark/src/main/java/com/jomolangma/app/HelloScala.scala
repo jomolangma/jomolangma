c48fbb8ab9c0750d2913936b6ebf4de0d3602972
jomolangma <zhanglijun@jomolangma.com>
2015-06-25 12:00:51 +0800

    scala
    scla

--- app-spark/src/main/java/com/jomolangma/app/HelloScala.scala
+++ app-spark/src/main/java/com/jomolangma/app/HelloScala.scala
@@ -6,3 +6,27 @@ package com.jomolangma.app
 class HelloScala {
 
 }
+object HelloScala {
+
+  def main (args: Array[String]) {
+    println("Hello Scala!!!")
+    //println(hello("Scala"))
+    //helloScala()
+    //println(add(1,2))
+    //println(sum2(2)(3))
+    //variableParameter("I","Live","Spark")
+    //println(helloDefault())
+
+    var (n,r) = (10,0)
+    while(n>0){
+      r = r + n
+      n = n - 1
+    }
+
+    println(r)
+
+    for (i<- 1 to 10 if i%2 == 0){
+      println(i)
+    }
+  }
+}
