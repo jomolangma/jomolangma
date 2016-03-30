2cea3a9f343ea1f48d0282214c6d506e0392ee90
jomolangma <zhanglijun@jomolangma.com>
2016-03-30 23:13:16 +0800

    scala change

---
+++ app-spark/src/main/java/com/jomolangma/app/scala/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.jomolangma.app.scala;
+
+import com.jomolangma.app.scala.Hello;
+
+/**
+ * Created by ZhangLijun on 3/30/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        Hello h = new Hello();
+        h.sayHello("scala");
+    }
+}
---
+++ app-spark/src/main/scala/com/jomolangma/app/scala/Hello.scala
@@ -0,0 +1,10 @@
+package com.jomolangma.app.scala
+
+/**
+  * Created by ZhangLijun on 3/30/16.
+  */
+class Hello {
+  def sayHello(x: String): Unit = {
+    println("hello," + x);
+  }
+}
